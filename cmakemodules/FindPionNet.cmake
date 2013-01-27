4a8e075b3620c6d2105af36e9845e1857c06eaec
Dom De Re <domdere@domdere.com>
2013-01-28 02:01:29 +1100

    fixed up the pion net find module so that it works, it looks like the original one was expecting an older version of Pion or possibly the enterprise one as the header it was looking for was not part of my installation, nor was the common library

--- cmakemodules/FindPionNet.cmake
+++ cmakemodules/FindPionNet.cmake
@@ -4,7 +4,7 @@
 #  PIONNET_LIBRARIES
 #  PIONNET_INCLUDE_DIR
 
-FIND_PATH(PIONNET_INCLUDE_DIR pion/PionConfig.hpp
+FIND_PATH(PIONNET_INCLUDE_DIR pion/logger.hpp
   HINTS
   $ENV{PIONNET_DIR}
   PATH_SUFFIXES include
@@ -20,7 +20,7 @@ FIND_PATH(PIONNET_INCLUDE_DIR pion/PionConfig.hpp
 )
 
 FIND_LIBRARY(PIONNET_LIBRARY
-    NAMES pion-net
+    NAMES pion-net libpion.a
     HINTS
     $ENV{PIONNET_DIR}
     PATH_SUFFIXES lib64 lib
@@ -33,27 +33,36 @@ FIND_LIBRARY(PIONNET_LIBRARY
     /opt/local
     /opt/csw
     /opt
-    )
-
-FIND_LIBRARY(PIONCOMMON_LIBRARY
-  NAMES pion-common
-  HINTS
-  $ENV{PIONNET_DIR}
-  PATH_SUFFIXES lib64 lib
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw
-  /opt/local
-  /opt/csw
-  /opt
 )
 
-IF(PIONNET_LIBRARY AND PIONCOMMON_LIBRARY)
-    SET(PIONNET_LIBRARIES debug ${PIONNET_LIBRARY} ${PIONCOMMON_LIBRARY} optimized ${PIONNET_LIBRARY} ${PIONCOMMON_LIBRARY} CACHE STRING "PionNet Libraries")
-ENDIF()
+IF ( PIONNET_DEBUG )
+    MESSAGE ( STATUS "Pion library: ${PIONNET_LIBRARY}, Pion Include: ${PIONNET_INCLUDE_DIR}" )
+ENDIF ( PIONNET_DEBUG )
+
+#FIND_LIBRARY(PIONCOMMON_LIBRARY
+#  NAMES pion-common
+#  HINTS
+#  $ENV{PIONNET_DIR}
+#  PATH_SUFFIXES lib64 lib
+#  PATHS
+#  ~/Library/Frameworks
+#  /Library/Frameworks
+#  /usr/local
+#  /usr
+#  /sw
+#  /opt/local
+#  /opt/csw
+#  /opt
+#)
+
+#IF(PIONNET_LIBRARY AND PIONCOMMON_LIBRARY)
+#    SET(PIONNET_LIBRARIES debug ${PIONNET_LIBRARY} ${PIONCOMMON_LIBRARY} optimized ${PIONNET_LIBRARY} ${PIONCOMMON_LIBRARY} CACHE STRING "PionNet Libraries")
+#ENDIF()
+
+
+IF ( PIONNET_LIBRARY )
+    SET ( PIONNET_LIBRARIES debug ${PIONNET_LIBRARY} optimized ${PIONNET_LIBRARY} CACHE STRING "PionNet Libraries" )
+ENDIF ( PIONNET_LIBRARY )
 
 INCLUDE(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set PIONNET_FOUND to TRUE if
