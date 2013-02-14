1fcea8b80a39933e037b4d08b0e6029fc2c436b7
Dom De Re <domdere@domdere.com>
2013-02-14 16:31:34 +1100

    got all the cmake find packages running for linux

--- cmakemodules/FindJsonSpirit.cmake
+++ cmakemodules/FindJsonSpirit.cmake
@@ -5,6 +5,13 @@
 #  Json_Spirit_LIBRARY_DIR
 #  Json_Spirit_LIBRARY
 
+
+OPTION ( JSON_SPIRIT_DEBUG "Debug mode for Json spirit find package" OFF )
+
+# defaults to ON because the default "make install" target for json built and installed
+# static libs, library seems small and lightweight enough to warrant stati libs too
+OPTION ( JSON_SPIRIT_USE_STATIC_LIBS "Options to use static json_spirit libs" ON )
+
 FIND_PATH( Json_Spirit_INCLUDE_DIR json_spirit.h
     HINTS
     $ENV{JSONSPIRIT_DIR}
@@ -20,7 +27,25 @@ FIND_PATH( Json_Spirit_INCLUDE_DIR json_spirit.h
     /opt )
 
 # right now its only geared towards the common linux distros...
-FIND_PATH( Json_Spirit_LIBRARY_DIR json_spirit
+
+IF ( MSVC )
+    # dont know what the library is named in windows i'll add it here once i give it a try...
+ELSE ( MSVC )
+
+    IF ( JSON_SPIRIT_USE_STATIC_LIBS )
+        SET ( JSON_LIBRARY_NAME "libjson_spirit.a" )
+    ELSE ( JSON_SPIRIT_USE_STATIC_LIBS )
+        # not sure about this one either
+        SET ( JSON_LIBRARY_NAME "libjson_spirit.so" )
+    ENDIF ( JSON_SPIRIT_USE_STATIC_LIBS )
+
+ENDIF ( MSVC )
+
+IF (JSON_SPIRIT_DEBUG)
+    MESSAGE ( STATUS "Looking for: ${JSON_LIBRARY_NAME}" )
+ENDIF (JSON_SPIRIT_DEBUG)
+
+FIND_PATH( Json_Spirit_LIBRARY_DIR ${JSON_LIBRARY_NAME}
     HINTS
     $ENV{JSONSPIRIT_DIR}
     PATH_SUFFIXES lib lib/json_spirit lib64 lib64/json_spirit
@@ -29,7 +54,7 @@ FIND_PATH( Json_Spirit_LIBRARY_DIR json_spirit
     /usr )
 
 # right now its only geared towards the common linux distros...
-FIND_LIBRARY( Json_Spirit_LIBRARY json_spirit
+FIND_LIBRARY( Json_Spirit_LIBRARY ${JSON_LIBRARY_NAME}
     HINTS
     $ENV{JSONSPIRIT_DIR}
     PATH_SUFFIXES lib lib/json_spirit lib64 lib64/json_spirit
@@ -39,6 +64,7 @@ FIND_LIBRARY( Json_Spirit_LIBRARY json_spirit
 
 
 INCLUDE(FindPackageHandleStandardArgs)
+
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Json_Spirit DEFAULT_MSG 
 	Json_Spirit_INCLUDE_DIR 
     Json_Spirit_LIBRARY_DIR 
