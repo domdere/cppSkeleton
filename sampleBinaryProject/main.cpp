086bebea025353b71ba1d5a9243a658dd1024d9f
Dom De Re <domdere@domdere.com>
2013-07-24 20:49:35 +1000

    got rid of the test garbage i put in the app stub

--- sampleBinaryProject/main.cpp
+++ sampleBinaryProject/main.cpp
@@ -1,14 +1,7 @@
 #include "appFW/consoleAppMain.hpp"
 #include "profiny/profiny.hpp"
 
-#include "parallel/transform.hpp"
-
-#include <boost/range/algorithm/generate.hpp>
-#include <boost/range/algorithm/transform.hpp>
-
-#include <array>
 #include <iostream>
-#include <vector>
 
 namespace projectnamespace {
 
@@ -24,27 +17,6 @@ std::string GetApplicationName()
 int AppMain(const po::variables_map& vm)
 {
     PROFINY_SCOPE
-
-    std::vector<boost::uint32_t> mTest(1000000);
-
-    boost::uint32_t counter = 0u;
-
-    boost::generate(mTest, [&counter]() 
-    {
-        return counter++;
-    });
-
-    std::array<boost::uint32_t, 1000000> outArray;
-
-    {
-        PROFINY_SCOPE
-        parallel::transform(mTest, outArray.data(), [] (boost::uint32_t in) -> boost::uint32_t
-        {
-            return 2 * in;
-        },
-        4,
-        10);
-    }
 
     return 0;
 }
